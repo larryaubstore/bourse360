9245507b1788b162ca68c01533b4e9afde7dea3c
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2013-11-28 21:01:38 -0500

    Heroku cleaning.

--- app/routes/userchart/db.js
+++ app/routes/userchart/db.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
-var config = require('../../config');
+//var config = require('../../config');
 
-var mongoURI = ( process.env.PORT ) ? config.creds.mongoose_auth_jitsu : config.creds.mongoose_auth_local;
-var db = mongoose.connect(mongoURI);
+//var mongoURI = ( process.env.PORT ) ? config.creds.mongoose_auth_jitsu : config.creds.mongoose_auth_local;
+//var db = mongoose.connect(mongoURI);
 
 
-exports.db = db;
+//exports.db = db;
 
